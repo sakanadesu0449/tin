efc8fe3117f4a6b89f92c1b0755c0c9fcb9e5f59
Sakana0449 <sakanadesu255378343@gmail.com>
2025-03-23 04:29:43 +0800

    '0.0.4 format'

--- src/tintypes.cpp
+++ src/tintypes.cpp
@@ -1,27 +1,22 @@
 #include "tintypes.hpp"
 
 
-TinTrack* TinPlaylist::get_track(const std::string& trackname)
-{
+TinTrack* TinPlaylist::get_track(const std::string& trackname) {
     auto track = tracklist.find(trackname);
-    if (track != tracklist.end())
-    {
+    if (track != tracklist.end()) {
         return &(track->second);
     }
     return nullptr;
 }
 
-void TinPlaylist::add_track(const TinTrack& track)
-{
+void TinPlaylist::add_track(const TinTrack& track) {
     tracklist.emplace(track.trackname, track);
 }
-bool TinPlaylist::del_track(const std::string& trackname)
-{
+bool TinPlaylist::del_track(const std::string& trackname) {
     return tracklist.erase(trackname) > 0;
 }
 
-size_t TinPlaylist::get_playlist_size() const
-{
+size_t TinPlaylist::get_playlist_size() const {
     return tracklist.size();
 }
 
