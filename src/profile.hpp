c1db0530105ac8bf94ae1fbe04d7b692bde4f453
Sakana0449 <sakanadesu255378343@gmail.com>
2025-03-25 19:37:22 +0800

    0.0.6.01 bug fix

--- src/profile.hpp
+++ src/profile.hpp
@@ -46,8 +46,48 @@ public:
 protected:
 	std::unique_ptr<StreamType> file_stream;
 	nlohmann::json user_profile;
-	bool file_exist_checker(const std::string filepath);
-    std::any json_to_any(const nlohmann::json& _j);
+	bool file_exist_checker(const std::string filepath) {
+		std::filesystem::path _filepath(filepath);
+		std::filesystem::path _parent = _filepath.parent_path();
+	
+		if(!std::filesystem::exists(_parent)) {
+			if(!std::filesystem::create_directories(_parent)) {
+				std::cerr << "Failed to create user profile directories: " << filepath << std::endl;
+				return false;
+			}
+		}
+		return true;
+	}
+
+    std::any json_to_any(const nlohmann::json& _j) {
+		if (_j.is_number_integer()) {
+			return _j.get<int>();
+		} else if (_j.is_string()) {
+			return _j.get<std::string>();
+		} else if (_j.is_boolean()) {
+			return _j.get<bool>();
+		} else if (_j.is_number_unsigned()) {
+			return _j.get<unsigned int>();
+		} else if (_j.is_number_float()) {
+			return _j.get<double>();
+		} else if (_j.is_null()) {
+			return nullptr;
+		} else if (_j.is_array()) {
+			std::vector<std::any> o_array;
+			for (const auto& i : _j) {
+				o_array.push_back(json_to_any(i));
+			}
+			return o_array;
+		} else if (_j.is_object()) {
+			std::map<std::string, std::any> o_map;
+			for (const auto& [key, value] : _j.items()) {
+				o_map[key] = json_to_any(value);
+			}
+			return o_map;
+		} else {
+			throw(std::runtime_error("err: unsuppost json type"));
+		}
+	}
 };
 
 class ReadUserProfile : public ABCJsonHandler<std::ifstream> {
